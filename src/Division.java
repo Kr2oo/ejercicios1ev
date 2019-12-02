1b4e6b370d15e5ce148496c0fa69fa5d07c554e6
Kr2o <firestorm1x@gmail.com>
2019-12-02 20:28:05 +0100

    Ejercicio 1 Unidad 2

---
+++ src/Division.java
@@ -0,0 +1,10 @@
+
+public class Division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num1=1234, num2=523;
+		System.out.printf("1234 / 532 = %15.2f", (num1 / num2));
+	}
+
+}
